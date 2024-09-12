2eac2326c56352ee9533e2f104e302e27bb97a9a
Ruslan <sounditbox@gmail.com>
2024-09-12 22:08:28 +0400

    ready third task

--- Masha.py
+++ Masha.py
@@ -1,4 +1,5 @@
 print("Продолжение темы для выполнения домашки :)")
 print('Чтобы лучше понять человека, поставь себя на его место.')
-
+print('Simple is better than complex')
+print('Complex is better than complicated')
 """Каждый делится тем, чем сам богат."""
