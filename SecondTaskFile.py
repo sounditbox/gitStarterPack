92e3aee816dc5aa3293163c003dd7ac41d2abd1f
Kirill Olkhovskii <kirill.olkhovskii@gmail.com>
2024-09-20 23:29:06 -0400

    second task commit

---
+++ SecondTaskFile.py
@@ -0,0 +1,9 @@
+import math
+print("Велком на второе задание!")
+a = 20
+b = math.factorial(a)
+c = math.pi * a ** 2
+print(f"лалалашечки = {a}")
+print(f"трололошки = {b}")
+print(f"усипуси = {c}")
+
